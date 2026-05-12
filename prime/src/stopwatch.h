2ebde272f8848c77d3cfdb25952a6767748fffe5
Amy Babay <amy.e.babay@gmail.com>
2026-05-12 17:50:31 -0400

    fix prime license links

--- prime/src/stopwatch.h
+++ prime/src/stopwatch.h
@@ -6,7 +6,7 @@
  * this file except in compliance with the License.  You may obtain a
  * copy of the License at:
  *
- * http://www.dsn.jhu.edu/prime/LICENSE.txt
+ * https://jhu-dsn.github.io/prime/LICENSE.txt
  *
  * or in the file ``LICENSE.txt'' found in this distribution.
  *
